fa8b108c80a0ead248698062e7a773b14d7f4bd3
@anonimo <124482149+srcarneiro1@users.noreply.github.com>
2026-08-18 11:47:06 -0300

    fix: prioriza tema visual Unilog

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './planning.css'
-createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>)
+import './brand.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
