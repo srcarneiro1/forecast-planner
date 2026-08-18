0dfe3bed26d5a6fa549f6ecd80a007dd1e933cc5
@anonimo <124482149+srcarneiro1@users.noreply.github.com>
2026-08-18 12:40:33 -0300

    fix: carrega tema Unilog como camada final

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './planning.css'
 import './brand.css'
+import './unilog.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
