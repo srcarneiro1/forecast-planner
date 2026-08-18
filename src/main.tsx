d2c1194cc40634a28fd46d2a6de348420eac4f1f
@anonimo <124482149+srcarneiro1@users.noreply.github.com>
2026-08-18 01:25:34 -0300

    load planning decision styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './planning.css'
 createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>)
