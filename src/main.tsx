2bad729dd641be341c0dc3c084e0d170b65896ac
@anonimo <124482149+srcarneiro1@users.noreply.github.com>
2026-08-18 12:49:38 -0300

    fix: carrega ajuste final dos logos

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './planning.css'
 import './brand.css'
 import './unilog.css'
+import './logo.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
