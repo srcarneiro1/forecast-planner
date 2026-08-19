d3a7b6534cf834a732e71e5e70ec3cd48b60dce0
@anonimo <124482149+srcarneiro1@users.noreply.github.com>
2026-08-19 15:16:32 -0300

    brand: remove legacy logo css overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import App from './App'
 import './planning.css'
 import './brand.css'
 import './unilog.css'
-import './logo.css'
 import './auth.css'
-import './brand-final.css'
 import './brand-logo-final.css'
 
 createRoot(document.getElementById('root')!).render(
